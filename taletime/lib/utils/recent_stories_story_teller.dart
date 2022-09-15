bb1bd97b44e22b193e5d936ac3db1557bc9ddbdf
Gianluca Goebel <ggoebel@htw-saarland.de>
2022-09-15 18:44:23 +0200

    WIP: horizontale Liste-> funktioniert noch nicht!!

---
+++ taletime/lib/utils/recent_stories_story_teller.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:taletime/utils/constants.dart';
+
+
+///
+///
+///
+///
+///
+///
+///
+///
+///
+/// FUNKTIONIERT MOMENTAN NICHT!!!!!!!!!
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+
+
+
+class LVRecentStoryTeller extends StatefulWidget {
+  final List stories;
+  LVRecentStoryTeller(this.stories, {Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _LVRecentStoryTellerState(this.stories);
+  }
+}
+
+class _LVRecentStoryTellerState extends State<LVRecentStoryTeller> {
+  late final List stories;
+  _LVRecentStoryTellerState(this.stories);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        primary: false,
+        scrollDirection: Axis.horizontal,
+        itemCount: stories.length,
+        itemBuilder: (_, index) {
+          return Card(
+            child: SizedBox(
+              height: 150,
+              width: 150,
+              child: ListTile(
+                leading: Image.network(stories[index]["image"] == ""
+                    ? storyImagePlaceholder
+                    : stories[index]["image"]),
+                title: Text(stories[index]["title"]),
+                subtitle: Text(stories[index]["author"]),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.upload),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.edit),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.delete),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
