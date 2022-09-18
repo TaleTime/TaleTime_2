9a85a0a9395d12c11b4edcbca73e5eb63482ef32
Martin Fokam Simo <martin.fsimo@gmail.com>
2022-09-18 16:22:15 +0200

    update all stories page with adequate code standards(collection references)

--- taletime/lib/screens/all_stories.dart
+++ taletime/lib/screens/all_stories.dart
@@ -1,25 +1,80 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:taletime/utils/constants.dart';
-import '../utils/my_list_view.dart';
-import '../utils/search-bar-util.dart';
 
 class AllStories extends StatefulWidget {
-  final List stories;
-  const AllStories(this.stories, {Key? key}) : super(key: key);
+  final CollectionReference recordedStoriesCollection;
+  const AllStories(this.recordedStoriesCollection, {Key? key}) : super(key: key);
 
   @override
-  State<AllStories> createState() => _AllStoriesState(this.stories);
+  State<AllStories> createState() => _AllStoriesState(this.recordedStoriesCollection);
 }
 
 class _AllStoriesState extends State<AllStories> {
-  final List stories;
-  _AllStoriesState(this.stories);
+  final CollectionReference recordedStoriesCollection;
+  _AllStoriesState(this.recordedStoriesCollection);
 
   String name = "AllStories";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return StreamBuilder(
+        stream: recordedStoriesCollection.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            final List<QueryDocumentSnapshot> recordedStoriesDocumentSnapshot = streamSnapshot.data!.docs;
+            return Scaffold(
+                appBar: AppBar(
+                  title: Text(
+                    "All Stories",
+                  ),
+                  automaticallyImplyLeading: false,
+                ),
+                body: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 500,
+                        child: ListView.builder(
+                            itemCount: recordedStoriesDocumentSnapshot.length,
+                            itemBuilder: (context, index) {
+                              return Card(
+                                child: ListTile(
+                                  title: Text(recordedStoriesDocumentSnapshot[index]["title"]),
+                                  subtitle: Text(recordedStoriesDocumentSnapshot[index]["author"]),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.upload),
+                                      ),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.edit),
+                                      ),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: const Icon(Icons.delete),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            }),
+                      )
+                    ],
+                  ),
+                ));
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        }
+        );
+
+     /* Scaffold(
         appBar: AppBar(
           title: Text(
             "All Stories",
@@ -62,6 +117,6 @@ class _AllStoriesState extends State<AllStories> {
               )
             ],
           ),
-        ));
+        ));*/
   }
 }
