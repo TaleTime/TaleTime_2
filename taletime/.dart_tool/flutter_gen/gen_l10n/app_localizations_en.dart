c4a7a222a312d800e955e8ed62ed18a5b61be50a
Martin Fokam Simo <martin.fsimo@gmail.com>
2022-10-03 21:50:23 +0200

    merge main to main origin

---
+++ taletime/.dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,216 @@
+
+
+
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get welcomeToTaleTime => 'Welcome to TaleTime';
+
+  @override
+  String get descriptionWelcome => 'The cloud-ready, storytelling audiobook app for fun with the whole family';
+
+  @override
+  String get register => 'Sign Up';
+
+  @override
+  String get registerVerb => 'Sign Up';
+
+  @override
+  String get loginVerb => 'Sign In';
+
+  @override
+  String get login => 'Sign In';
+
+  @override
+  String get loginToAccount => 'Sign In to your Account';
+
+  @override
+  String get email => 'Email adress';
+
+  @override
+  String get enterEmail => 'Enter your email adress';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get enterPassword => 'Enter your password';
+
+  @override
+  String get forgotPassword => 'Forgot Password?';
+
+  @override
+  String get confirmPassword => 'Confirm password';
+
+  @override
+  String get confirmYourPassword => 'Confirm your password';
+
+  @override
+  String get passwordsDontMatch => 'The passwords don\'t match';
+
+  @override
+  String get dontHaveAccount => 'Don\'t have an Account?';
+
+  @override
+  String get createAccount => 'Create an account, it\'s free.';
+
+  @override
+  String get username => 'Username';
+
+  @override
+  String get enterUsername => 'Enter your username';
+
+  @override
+  String get alreadyHaveAccount => 'Already have an account?';
+
+  @override
+  String get resetPassword => 'Reset password';
+
+  @override
+  String get problemsWithLogin => 'Problems with Login?';
+
+  @override
+  String get enterAssociatedEmail => 'Enter the email address associated with your account.';
+
+  @override
+  String get sendResetLink => 'We will email you a link to reset your password.';
+
+  @override
+  String get rememberPassword => 'Remember password? ';
+
+  @override
+  String get emailRequired => 'Email adress is required';
+
+  @override
+  String get enterValidEmail => 'Enter a valid email adress';
+
+  @override
+  String get passwordRequired => 'Password is required';
+
+  @override
+  String get passwordLength => 'Password needs to be at least 6 characters long';
+
+  @override
+  String get usernameRequired => 'Username is required';
+
+  @override
+  String get usernameLength => 'Username needs to be at least 6 characters long';
+
+  @override
+  String get userNotFound => 'No user found with the associated email adress';
+
+  @override
+  String get wrongPassword => 'Wrong Password.';
+
+  @override
+  String get emailAlreadyInUse => 'Email already in use.';
+
+  @override
+  String get ok => 'Ok';
+
+  @override
+  String get yes => 'Yes';
+
+  @override
+  String get no => 'No';
+
+  @override
+  String get emailSent => 'Email was succesfully sent';
+
+  @override
+  String get myProfiles => 'My Profiles';
+
+  @override
+  String get loggingOut => 'Signing out out...';
+
+  @override
+  String get confirmLogout => 'Do you really want to sign out?';
+
+  @override
+  String get deleteProfile => 'Delete profile...';
+
+  @override
+  String get confirmProfileDeletion => 'Do you really want to delte this profile?';
+
+  @override
+  String get edit => 'Edit';
+
+  @override
+  String get delete => 'Delete';
+
+  @override
+  String get myAccount => 'My Account';
+
+  @override
+  String get changeProfile => 'Change Profile';
+
+  @override
+  String get signOut => 'Sign Out';
+
+  @override
+  String get signInSuccesful => 'Sign In succesful';
+
+  @override
+  String get signUpSuccesful => 'Sign Up succesful';
+
+  @override
+  String get mainMenu => 'Main Menu';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get favorites => 'Favorites';
+
+  @override
+  String get listenNow => 'Listen now';
+
+  @override
+  String get search => 'Search';
+
+  @override
+  String get changeLanguage => 'Change Language';
+
+  @override
+  String get darkMode => 'Dark Mode';
+
+  @override
+  String get changePassword => 'Change Password';
+
+  @override
+  String get welcome => 'Welcome';
+
+  @override
+  String get newProfile => 'New Profile';
+
+  @override
+  String get addProfile => 'Add Profile';
+
+  @override
+  String get listener => 'Listener';
+
+  @override
+  String get storyteller => 'Story-teller';
+
+  @override
+  String get titleRequired => 'Title is required';
+
+  @override
+  String get titleLength => 'Title needs to be at least 3 characters long';
+
+  @override
+  String get profileName => 'Profile-Name';
+
+  @override
+  String get enterProfile => 'Enter a profile name';
+
+  @override
+  String get oldPassword => 'Old password';
+
+  @override
+  String get enterOldPassword => 'Enter Old password';
+}
