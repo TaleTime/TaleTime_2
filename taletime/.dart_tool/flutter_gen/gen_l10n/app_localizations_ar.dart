ae10c6e856f0d8f4f298710487cea067e35547ae
Martin Fokam Simo <martin.fsimo@gmail.com>
2022-10-03 21:50:23 +0200

    merge main to main origin

---
+++ taletime/.dart_tool/flutter_gen/gen_l10n/app_localizations_ar.dart
@@ -0,0 +1,216 @@
+
+
+
+import 'app_localizations.dart';
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get welcomeToTaleTime => 'مرحبا بكم في ';
+
+  @override
+  String get descriptionWelcome => 'Das cloudfähige, geschichtenerzählende Vorlesetool für Spaß mit der ganzen Familie';
+
+  @override
+  String get register => 'انشاء حساب';
+
+  @override
+  String get registerVerb => 'انشاء حساب';
+
+  @override
+  String get loginVerb => 'تسجيل';
+
+  @override
+  String get login => 'التسجيل';
+
+  @override
+  String get loginToAccount => 'تسجيل في حسابي';
+
+  @override
+  String get email => 'عنوان الايميل';
+
+  @override
+  String get enterEmail => 'ادخل الايميل';
+
+  @override
+  String get password => 'كلمه السر';
+
+  @override
+  String get enterPassword => 'ادخل كلمه السر';
+
+  @override
+  String get forgotPassword => 'نسيت كلمه السر';
+
+  @override
+  String get confirmPassword => 'تاكيد كلمه السر';
+
+  @override
+  String get confirmYourPassword => 'تاكيد كلمه السر ';
+
+  @override
+  String get passwordsDontMatch => 'كلمه السر غير مطابقة';
+
+  @override
+  String get dontHaveAccount => 'انشاء حساب مجانا؟';
+
+  @override
+  String get createAccount => 'انشاء حساب مجانا';
+
+  @override
+  String get username => 'اسم المستخدم ';
+
+  @override
+  String get enterUsername => 'ادخل اسم المستخدم ';
+
+  @override
+  String get alreadyHaveAccount => 'لديك حساب سابق';
+
+  @override
+  String get resetPassword => 'ارجع كلمه السر';
+
+  @override
+  String get problemsWithLogin => 'مشاكل مع التسجيل';
+
+  @override
+  String get enterAssociatedEmail => 'ادخل الايميل المرتبط بالحساب';
+
+  @override
+  String get sendResetLink => ' نحن نرسل لك رابط لعنوان الايميل المرتبط بهذا الحساب تستطيع استرداد كلمه السر   ';
+
+  @override
+  String get rememberPassword => 'كلمه السر حقا لم تنسها?';
+
+  @override
+  String get emailRequired => 'Email-Adresse ist erforderlich';
+
+  @override
+  String get enterValidEmail => 'Geben Sie eine gültige Email-Adresse ein';
+
+  @override
+  String get passwordRequired => 'Passwort ist erforderlich';
+
+  @override
+  String get passwordLength => 'Passwort muss mindestens 6 Zeichen lang sein';
+
+  @override
+  String get usernameRequired => 'Benutzername ist erforderlich';
+
+  @override
+  String get usernameLength => 'Benutzername muss mindestens 6 Zeichen lang sein';
+
+  @override
+  String get userNotFound => 'Es wurde kein Nutzer mit der eingegebenen Email gefunden.';
+
+  @override
+  String get wrongPassword => 'Falsches Passwort.';
+
+  @override
+  String get emailAlreadyInUse => 'Es existiert bereits ein Nutzer mit der eingegebenen Email-Adresse.';
+
+  @override
+  String get ok => 'Ok';
+
+  @override
+  String get yes => 'Ja';
+
+  @override
+  String get no => 'Nein';
+
+  @override
+  String get emailSent => 'Email wurde erfolgreich versendet';
+
+  @override
+  String get myProfiles => 'Meine Profile';
+
+  @override
+  String get loggingOut => 'Abmelden...';
+
+  @override
+  String get confirmLogout => 'Wollen Sie sich wirklich vom Ihrem Konto abmelden?';
+
+  @override
+  String get deleteProfile => 'Profil löschen...';
+
+  @override
+  String get confirmProfileDeletion => 'Wollen Sie wirklich dieses Profil löschen?';
+
+  @override
+  String get edit => 'Bearbeiten';
+
+  @override
+  String get delete => 'Löschen';
+
+  @override
+  String get myAccount => 'Mein Konto';
+
+  @override
+  String get changeProfile => 'Profil wechseln';
+
+  @override
+  String get signOut => 'Abmelden';
+
+  @override
+  String get signInSuccesful => 'Anmelden erfolgreich';
+
+  @override
+  String get signUpSuccesful => 'Registrieren erfolgreich';
+
+  @override
+  String get mainMenu => 'Hauptmenü';
+
+  @override
+  String get settings => 'Einstellungen';
+
+  @override
+  String get favorites => 'Favoriten';
+
+  @override
+  String get listenNow => 'Jetzt anhören';
+
+  @override
+  String get search => 'Suchen';
+
+  @override
+  String get changeLanguage => 'Sprache umstellen';
+
+  @override
+  String get darkMode => 'Dark Mode';
+
+  @override
+  String get changePassword => 'Passwort ändern';
+
+  @override
+  String get welcome => 'Willkommen';
+
+  @override
+  String get newProfile => 'Neues Profil';
+
+  @override
+  String get addProfile => 'Profil hinzufügen';
+
+  @override
+  String get listener => 'Hörer';
+
+  @override
+  String get storyteller => 'Erzähler';
+
+  @override
+  String get titleRequired => 'Titel ist erforderlich';
+
+  @override
+  String get titleLength => 'Titel muss mindestens 3 Zeichen lang sein';
+
+  @override
+  String get profileName => 'Profil-Name';
+
+  @override
+  String get enterProfile => 'Geben Sie einen Profil-Namen ein';
+
+  @override
+  String get oldPassword => 'Altes Passwort';
+
+  @override
+  String get enterOldPassword => 'Geben Sie Ihr altes Passwort ein';
+}
