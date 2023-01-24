5ca7051a1b1c0652d7609fad03417a31b95ed451
SeScherer <44403634+SeScherer@users.noreply.github.com>
2023-01-24 20:37:05 +0100

    added Logger

---
+++ taletime/lib/common utils/tale_time_logger.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:logger/logger.dart';
+
+class TaleTimeLogger {
+  static Logger getLogger() {
+    return Logger(
+        level: kReleaseMode ? Level.warning : Level.debug,
+        printer: PrettyPrinter(
+            methodCount: 2, // number of method calls to be displayed
+            errorMethodCount: 8, // number of method calls if stacktrace is provided
+            lineLength: 120, // width of the output
+            colors: true, // Colorful log messages
+            printEmojis: true, // Print an emoji for each log message
+            printTime: false // Should each log print contain a timestamp
+
+            ));
+  }
+}
