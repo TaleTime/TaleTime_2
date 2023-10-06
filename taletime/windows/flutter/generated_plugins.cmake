5f3757edf631cc43950091450d751b291a039c00
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-10-06 20:21:40 +0200

    Fixed Record declaration.

--- taletime/windows/flutter/generated_plugins.cmake
+++ taletime/windows/flutter/generated_plugins.cmake
@@ -4,7 +4,11 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
+  file_selector_windows
+  firebase_auth
+  firebase_core
   permission_handler_windows
+  record_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
