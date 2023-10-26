5360385e3661fedf2ae85a278134bbdb8e61815f
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2023-10-26 13:48:17 +0200

    Fix: Center welcome screen and remove appbar.

--- taletime/lib/login and registration/screens/welcome.dart
+++ taletime/lib/login and registration/screens/welcome.dart
@@ -11,96 +11,93 @@ class WelcomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var size = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text(AppLocalizations.of(context)!.welcome),
-      ),
       body: SafeArea(
-        child:
-        SingleChildScrollView(
-        child: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.fromLTRB(20, 30, 20, 0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
-                        child: Center(
-                      child: Text(
-                        textAlign: TextAlign.center,
-                    AppLocalizations.of(context)!.loginToTaleTime,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                    ),
-                  ))),
-          Padding(padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
-                  child: Text(
-                    AppLocalizations.of(context)!.descriptionWelcome,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 15,
-                    ),
-              )),
-                  SizedBox(
-                height:  200,
-                child: Image.asset("assets/logo.png"),
-              ),
-          Padding(padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                child: Column(
-                children: <Widget>[
-                  Padding(padding: const EdgeInsets.symmetric(vertical: 20),
-                  child:
-                  /// Login Button
-                  SizedBox(
-                    height: 50,
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: elevatedButtonDefaultStyle(),
+          child:
+          Center(
+              child: SingleChildScrollView(
+                  child: Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Padding(padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                                  child: Center(
+                                      child: Text(
+                                        textAlign: TextAlign.center,
+                                        AppLocalizations.of(context)!.welcomeToTaleTime,
+                                        style: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 30,
+                                        ),
+                                      ))),
+                              Padding(padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 20),
+                                  child: Text(
+                                    AppLocalizations.of(context)!.descriptionWelcome,
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                      fontSize: 15,
+                                    ),
+                                  )),
+                              SizedBox(
+                                height:  200,
+                                child: Image.asset("assets/logo.png"),
+                              ),
+                              Padding(padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                                child: Column(
+                                  children: <Widget>[
+                                    Padding(padding: const EdgeInsets.symmetric(vertical: 20),
+                                        child:
+                                        /// Login Button
+                                        SizedBox(
+                                          height: 50,
+                                          width: double.infinity,
+                                          child: ElevatedButton(
+                                            style: elevatedButtonDefaultStyle(),
 
-                      /// redirects the user to the LoginPage
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const LoginPage()));
-                      },
-                      child: Text(
-                        AppLocalizations.of(context)!.loginVerb,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.w600, fontSize: 18),
-                      ),
-                    ),
-                  )),
+                                            /// redirects the user to the LoginPage
+                                            onPressed: () {
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) => const LoginPage()));
+                                            },
+                                            child: Text(
+                                              AppLocalizations.of(context)!.loginVerb,
+                                              style: const TextStyle(
+                                                  fontWeight: FontWeight.w600, fontSize: 18),
+                                            ),
+                                          ),
+                                        )),
 
-                  SizedBox(
-                      height: 50,
-                      width: double.infinity,
-                        child: ElevatedButton(
-                        style: elevatedButtonDefaultStyle(),
+                                    SizedBox(
+                                        height: 50,
+                                        width: double.infinity,
+                                        child: ElevatedButton(
+                                          style: elevatedButtonDefaultStyle(),
 
-                        /// redirects the user to the SignupPage
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignupPage()));
-                        },
-                        child: Text(
-                          AppLocalizations.of(context)!.register,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.w600, fontSize: 18),
-                        ),
-                      )),
-                ],
-              ),
-              )]),
-        ),
-      ),
-    ));
+                                          /// redirects the user to the SignupPage
+                                          onPressed: () {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) => const SignupPage()));
+                                          },
+                                          child: Text(
+                                            AppLocalizations.of(context)!.register,
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.w600, fontSize: 18),
+                                          ),
+                                        )),
+                                  ],
+                                ),
+                              )])],
+                      ))))),
+    );
   }
 }
+
