0164af1e8564389ded14a862ae4d2c2f1ecc16c0
doba16 <92383352+doba16@users.noreply.github.com>
2023-10-29 23:33:09 +0100

    chore: Pushed signup button to bottom of screen

--- taletime/lib/login and registration/screens/login.dart
+++ taletime/lib/login and registration/screens/login.dart
@@ -43,134 +43,146 @@ class _LoginPageState extends State<LoginPage> {
         context: context,
         automaticArrow: true,
       ),
-      body: SizedBox(
-        height: double.infinity,
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(8, 20, 8, 20),
-                child: Text(
-                  AppLocalizations.of(context)!.loginToAccount,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height / 6,
-                child: Image.network(assetLogo),
-              ),
-              const SizedBox(height: 20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    children: <Widget>[
-                      /// TextField that catches the user input for the email-adress
-                      Container(
-                        width: double.infinity,
-                        decoration: Decorations().inputBoxDecorationShaddow(),
-                        child: TextFormFieldUtil().enterEmailForm(
-                          context,
-                          _emailController,
-                          TextInputAction.next,
+      body: LayoutBuilder(
+        builder: (context, constraints) => SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraints.maxHeight,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(8, 20, 8, 20),
+                        child: Text(
+                          AppLocalizations.of(context)!.loginToAccount,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                       const SizedBox(
-                        height: 20,
+                        height: 5,
                       ),
-
-                      /// TextField that catches the user input for the password
-                      Container(
-                        decoration: Decorations().inputBoxDecorationShaddow(),
-                        child: TextFormFieldUtil().enterPasswordForm(
-                          context,
-                          _passwordController,
-                          TextInputAction.done,
-                        ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height / 6,
+                        child: Image.network(assetLogo),
                       ),
-                      Container(
-                        margin: const EdgeInsets.fromLTRB(10, 2, 5, 15),
-                        alignment: Alignment.topRight,
-                        child: TextButton(
-                          child: Text(
-                              AppLocalizations.of(context)!.forgotPassword,
-                              style: TextStyle(color: kPrimaryColor)),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    ForgotPasswordPage(
-                                      initialEmail: _emailController.text,
-                                    ),
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Form(
+                          key: _formKey,
+                          child: Column(
+                            children: <Widget>[
+                              /// TextField that catches the user input for the email-adress
+                              Container(
+                                width: double.infinity,
+                                decoration: Decorations().inputBoxDecorationShaddow(),
+                                child: TextFormFieldUtil().enterEmailForm(
+                                  context,
+                                  _emailController,
+                                  TextInputAction.next,
+                                ),
                               ),
-                            );
-                          },
+                              const SizedBox(
+                                height: 20,
+                              ),
+
+                              /// TextField that catches the user input for the password
+                              Container(
+                                decoration: Decorations().inputBoxDecorationShaddow(),
+                                child: TextFormFieldUtil().enterPasswordForm(
+                                  context,
+                                  _passwordController,
+                                  TextInputAction.done,
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.fromLTRB(10, 2, 5, 15),
+                                alignment: Alignment.topRight,
+                                child: TextButton(
+                                  child: Text(
+                                      AppLocalizations.of(context)!.forgotPassword,
+                                      style: TextStyle(color: kPrimaryColor)),
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            ForgotPasswordPage(
+                                              initialEmail: _emailController.text,
+                                            ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: SizedBox(
+                            width: double.infinity,
+                            //Button for the Login
+                            child: ElevatedButton(
+                              style: elevatedButtonDefaultStyle(),
+
+                              /// logs in the user with the entered email and password
+                              /// if the input isn't valid, the the user will be informed with a error message under the belonging Textfield
+                              onPressed: () async {
+                                final String email =
+                                _emailController.text.trim().toLowerCase();
+                                final String password = _passwordController.text.trim();
+                                final isValidForm = _formKey.currentState!.validate();
+                                if (isValidForm) {
+                                  AuthentificationUtil(auth: auth)
+                                      .loginUsingEmailAndPassword(
+                                      email: email,
+                                      password: password,
+                                      context: context);
+                                }
+                              },
+                              child: Text(
+                                AppLocalizations.of(context)!.loginVerb,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w600, fontSize: 18),
+                              ),
+                            )),
                       ),
                     ],
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: SizedBox(
-                    width: double.infinity,
-                    //Button for the Login
-                    child: ElevatedButton(
-                      style: elevatedButtonDefaultStyle(),
 
-                      /// logs in the user with the entered email and password
-                      /// if the input isn't valid, the the user will be informed with a error message under the belonging Textfield
-                      onPressed: () async {
-                        final String email =
-                            _emailController.text.trim().toLowerCase();
-                        final String password = _passwordController.text.trim();
-                        final isValidForm = _formKey.currentState!.validate();
-                        if (isValidForm) {
-                          AuthentificationUtil(auth: auth)
-                              .loginUsingEmailAndPassword(
-                                  email: email,
-                                  password: password,
-                                  context: context);
-                        }
-                      },
-                      child: Text(
-                        AppLocalizations.of(context)!.loginVerb,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.w600, fontSize: 18),
-                      ),
-                    )),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(AppLocalizations.of(context)!.dontHaveAccount),
-                    TextButton(
-                        child: Text(AppLocalizations.of(context)!.registerVerb),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 40.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(AppLocalizations.of(context)!.dontHaveAccount),
+                        TextButton(
+                            child: Text(AppLocalizations.of(context)!.registerVerb),
 
-                        /// redirects the user to the SignupPage
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SignupPage()));
-                        }),
-                  ],
-                ),
+                            /// redirects the user to the SignupPage
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const SignupPage()));
+                            }),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
