6fcfdfe422f60603db22bcca67e26caa077d6d5c
doba16 <92383352+doba16@users.noreply.github.com>
2024-02-25 00:21:02 +0100

    fix: Fixed recently played stories

--- taletime/lib/player/screens/story_player.dart
+++ taletime/lib/player/screens/story_player.dart
@@ -1,5 +1,6 @@
 import "package:audio_service/audio_service.dart";
 import "package:flutter/material.dart";
+import "package:provider/provider.dart";
 import "package:taletime/internationalization/localizations_ext.dart";
 import "package:taletime/main.dart";
 import "package:taletime/player/widgets/player_controls.dart";
@@ -7,6 +8,7 @@ import "package:taletime/player/widgets/player_loading_spinner.dart";
 import "package:taletime/player/widgets/progress_bar.dart";
 import "package:taletime/player/widgets/story_image.dart";
 import "package:taletime/player/widgets/story_metadata.dart";
+import "package:taletime/state/profile_state.dart";
 
 import "../../common/models/added_story.dart";
 
@@ -27,6 +29,10 @@ class StoryPlayer extends StatelessWidget {
         "url": story.audioUrl ?? ""
       },
     ));
+
+    // Set timestamp
+    var storyRef = Provider.of<ProfileState>(context, listen: false).storiesRef!.doc(story.id);
+    storyRef.update({"timeLastListened": DateTime.now().millisecondsSinceEpoch});
   }
 
   @override
